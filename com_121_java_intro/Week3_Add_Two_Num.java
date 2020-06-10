f10b033495663e3e1db9ff720467b6b7c67e7d79
debaycisse <debaycisse@gmail.com>
2020-06-10 19:57:19 +0100

    week 3

--- com_121_java_intro/Week3_Add_Two_Num.java
+++ com_121_java_intro/Week3_Add_Two_Num.java
@@ -25,10 +25,14 @@ public class Week3_Add_Two_Num
 
 		// Declaration of non-primitive (reference) variable of type Scanner
 		Scanner input = new Scanner(System.in);
+		// Scanner is data type of reference type, so input is a variable of datatype Scanner
+		// Scanner() method is used to scan whatever object that is given to it's parameter
+		// System.in - is an object that hooks up system's input, such as keyboard.
+
 
 		// Accept values or data from user(s)
-		System.out.println("Enter first integer number please: ");
-		firstNumber = input.nextInt();
+		System.out.println("Enter first integer number please: ");  // informs user on what to type in.
+		firstNumber = input.nextInt();  // scans input stream and stores the value in d variable
 
 		System.out.println("Enter second integer number please: ");
 		secondNumber = input.nextInt();
