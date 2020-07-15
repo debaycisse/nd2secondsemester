61bff88675fc93618014d801fa623828cf2325b2
unknown <debaycisse@gmail.com>
2020-07-15 17:06:30 +0100

    week4 excercise and revision

--- com_121_java_intro/Week1.java
+++ com_121_java_intro/Week1.java
@@ -6,6 +6,6 @@ public class Week1
 {	// class definition header. This class' name is the same as file name but it just must not contain .java extension
 	public static void main(String[] args)
 	{	// main method header
-		System.out.println("Hello World!!!"); 
+		System.out.println("Hello World!!!");
 	}	// end of main method
 }	// end of HelloWorld class
---
+++ com_121_java_intro/Week4_Average.java
@@ -0,0 +1,51 @@
+/*
+* AverageThreeIntegers
+* Calculates the sumand average of any three integer numbers
+*/
+import javax.swing.JOptionPane;
+public class Week4_Average
+{
+	public static void main( String args[] )
+	{
+		int firstNumber; // first integer number
+		int secondNumber; // second integer number
+		int thirdNumber; // third integer number
+
+		int sum; // sum of the three numbers
+
+		double average; // average of the three numbers
+
+		String input; // input values
+		String result; // output generating string
+
+
+		// Accept inteher numbers from he user
+		input = JOptionPane.showInputDialog( null, "Enter first number: " );
+		firstNumber = Integer.parseInt( input ); // wrap input to integer
+		
+		input = JOptionPane.showInputDialog( null, "Enter second number: " );
+		secondNumber = Integer.parseInt( input ); // wrap input to integer
+		
+		input = JOptionPane.showInputDialog( null, "Enter third number: " );
+		thirdNumber = Integer.parseInt( input ); // wrap input to integer
+		
+
+		// Calculate sum
+		sum = firstNumber + secondNumber + thirdNumber;
+		
+
+		// Calculate average
+		average = sum/3.0;
+		
+
+		// Build output string and display output
+		result = "Average of " + firstNumber + ", " + secondNumber + " and " +
+		thirdNumber + " is = " + average;
+
+		JOptionPane.showMessageDialog( null, result, "Average of 3 Integers",
+		JOptionPane.INFORMATION_MESSAGE );
+
+
+	} // end method main
+
+} // end class AverageThreeIntegers
